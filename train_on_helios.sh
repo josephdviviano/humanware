6b375e24efc09c866a669a9e2bd2d63aace6ae75
Joseph Viviano <joseph@viviano.ca>
2019-03-09 19:23:48 -0500

    changed results directory (no longer just a generic path now that we use checkpointing, we have to point to the full experiment directory)

--- train_on_helios.sh
+++ train_on_helios.sh
@@ -4,11 +4,10 @@ export ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
 export SVHN_DIR='/rap/jvb-000-aa/COURS2019/etudiants/data/humanware/SVHN'
 export METADATA_FILENAME="${SVHN_DIR}/train_extra_metadata_split.pkl"
 
-
 s_exec python ${ROOT_DIR}'/train.py' \
     --dataset_dir=${SVHN_DIR} \
     --metadata_filename=${METADATA_FILENAME} \
-    --results_dir=${ROOT_DIR}/results \
+    --results_dir=${ROOT_DIR}/results/RESNET_FINAL \
     --cfg ${ROOT_DIR}/config/base_config.yml
 
 # echo "Copying files to local hard drive..."
